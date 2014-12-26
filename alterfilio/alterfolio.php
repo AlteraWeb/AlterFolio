ac1f0420eb5243106a24bb2a110009931b0322f2
Attar <attar@alteraweb.ru>
2014-12-26 14:56:12 +0200

    Create alterfolio.php

---
+++ alterfilio/alterfolio.php
@@ -0,0 +1,15 @@
+<?php
+/* ====================
+[BEGIN_SED_EXTPLUGIN]
+Code=AlterFolio
+Part=main
+File=AlterFolio
+Hooks=
+Tags=
+Order=10
+[END_SED_EXTPLUGIN]
+==================== */
+
+defined('SED_CODE') or die("Wrong URL.");
+
+?>
