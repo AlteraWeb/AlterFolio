224db363339308b361c26cd75bd84beabbe0da27
Attar <attar@alteraweb.ru>
2014-12-26 14:56:46 +0200

    Create alterfolio.setup.php

---
+++ alterfilio/alterfolio.setup.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * [BEGIN_COT_EXT]
+ * Code=AlterFolio
+ * Name=AlterFolio
+ * Category=Other
+ * Description=Полный перенос портфолио в карточку пользователя
+ * Version=1.0.1
+ * Date=
+ * Author=AlteraWeb Team
+ * Copyright=Copyright (c) AlteraWeb.ru
+ * Notes=
+ * Auth_guests=R
+ * Lock_guests=12345A
+ * Auth_members=RW
+ * Lock_members=12345A
+ * Requires_modules=folio
+ * Requires_plugins=
+ * [END_COT_EXT]
+ * 
+ */
+
+/**
+ * AlterFolio Plugin
+ *
+ * @package AlterFolio
+ * @version 1.0.1
+ * @author AlteraWeb Team
+ * @copyright Copyright (c) AlteraWeb.ru
+ * @license BSD
+ */
+
+?>
